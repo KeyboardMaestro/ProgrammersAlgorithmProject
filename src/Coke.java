0d817ab94df9b157a32f987e97011fa0bea1d41b
judepark <psw0616a@naver.com>
2022-11-22 23:38:33 +0900

    22, Nov, 2022

---
+++ src/Coke.java
@@ -0,0 +1,15 @@
+public class Coke {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a) {
+            answer += (n / a) * b;
+            n = (n % a) + (n / a) * b;
+        }
+        return answer;
+    }
+}
+
+//Resource : https://school.programmers.co.kr/learn/courses/30/lessons/132267
