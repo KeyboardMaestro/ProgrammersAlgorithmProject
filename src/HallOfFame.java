38b2f2703530be9f6c84f2d476146731fafa35ab
judepark <psw0616a@naver.com>
2022-11-24 20:00:13 +0900

    24, Nov, 2022 Solved run-time error

--- src/HallOfFame.java
+++ src/HallOfFame.java
@@ -7,14 +7,13 @@ public class HallOfFame {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         int[] rankings = new int[k];
-        Arrays.fill(rankings, 2001);
-        for (int i = 0; i < k ; i++){
-            rankings[i] = score[i];
-            answer[i] = Arrays.stream(rankings).min().getAsInt();
-        }
-        for (int i = k; i < score.length; i++){
+        Arrays.fill(rankings, 2000);
+        for (int i = 0; i < score.length; i++){
             rankings = Arrays.stream(rankings).sorted().toArray();
-            if (Arrays.stream(rankings).min().getAsInt() <= score[i])
+            if ( i < k){
+                rankings[i] = score[i];
+            }
+            else if (Arrays.stream(rankings).min().getAsInt() <= score[i])
                 rankings[0] = score[i];
             answer[i] = Arrays.stream(rankings).min().getAsInt();
         }
