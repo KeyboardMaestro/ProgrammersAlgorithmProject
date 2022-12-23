d12e0a13fcb0b99def2f64ccc54c3369b74079c5
judepark <psw0616a@naver.com>
2022-12-23 21:09:19 +0900

    23, Dec, 2022 Solved

---
+++ src/LifeBoat.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class LifeBoat {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int heavy = people.length - 1;
+        int light = 0;
+        int boat;
+        while (heavy >= light) {
+            boat = people[heavy--];
+            if (boat + people[light] <= limit) {
+                light++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
+
+// Resource : https://school.programmers.co.kr/learn/courses/30/lessons/42885#qna
