ea52bbb26c60e5a22c27da3fec96eae04238cb6c
judepark <psw0616a@naver.com>
2023-01-01 00:07:14 +0900

    1, Jan, 2023 First commit in 2023 - Happy new year!

---
+++ src/Protractor.java
@@ -0,0 +1,15 @@
+public class Protractor {
+    public int solution(int angle) {
+        int answer = 0;
+        if (angle == 180){
+            answer = 4;
+        } else if (angle == 90) {
+            answer = 2;
+        } else if (angle < 90){
+            answer = 1;
+        }else
+            answer = 3;
+        return answer;
+    }
+}
+// Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120829
