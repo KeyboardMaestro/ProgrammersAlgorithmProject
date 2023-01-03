2ac487d67d2dd3afd49bd28d26f01e2f71942880
judepark <psw0616a@naver.com>
2023-01-03 23:50:34 +0900

    3, Jan, 2023 Improve time complexity

--- src/programmers/algorithm/lv2/H_Index.java
+++ src/programmers/algorithm/lv2/H_Index.java
@@ -2,16 +2,12 @@ package programmers.algorithm.lv2;
 import java.util.Arrays;
 import java.util.Collections;
 public class H_Index {
-    public static void main(String[] args) {
-        int[] data = {3, 0, 6, 1, 5};
-        System.out.println(solution(data));
-    }
     public static int solution(int[] citations) {
         Integer[] data = Arrays.stream(citations).boxed().toArray(Integer[]::new);
         Arrays.sort(data, Collections.reverseOrder());// sorting data, Descending.
         int hPosition = 0; // Key value for computing h-index
         if (Arrays.stream(citations).min().getAsInt() <= data.length)
-            for (int i = 0 ; i < data[i]; i++){
+            for (int i = 0 ; i + 1 < data[i]; i++){
                 hPosition = i+1;
             }
         else {
