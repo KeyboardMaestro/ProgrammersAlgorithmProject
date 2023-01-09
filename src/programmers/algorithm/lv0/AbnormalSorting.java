42fca94ef8c3e944fa9bb89171f73ba74eacc858
judepark <psw0616a@naver.com>
2023-01-09 22:14:01 +0900

    Solved

--- src/programmers/algorithm/lv0/AbnormalSorting.java
+++ src/programmers/algorithm/lv0/AbnormalSorting.java
@@ -8,15 +8,15 @@ public class AbnormalSorting {
         solution(input, 4);
     }
     public static int[] solution(int[] numlist, int n) {
-        int[] answer = new int[numlist.length];
+        int[] answer = new int[numlist.length]; //
         Map<Integer,Integer> data = new HashMap<>();
         Arrays.sort(numlist);
         for (int i = 0; i < numlist.length; i++){
-            data.put(i,Math.abs( numlist[i] - n));
-        } // Map <값 , 키> 로 값에 "차의 절댓값", 키로 "주어진 수"
+            data.put(i, Math.abs( numlist[i] - n));
+        } // Map <키, 값> 로 값에 "차의 절댓값", 키로 "주어진 수"
 
         List<Integer> keySet = new ArrayList<>(data.keySet());
-        keySet.sort((o1, o2) -> data.get(o2).compareTo(data.get(o1)));
+        keySet.sort((o1, o2) -> data.get(o2).compareTo(data.get(o1))); // 데이터 값으로 비교해서 키 정렬...
         Collections.reverse(keySet);
 
         for (int i = 0; i < answer.length; i++){
---
+++ src/programmers/algorithm/lv1/EvenAndOdd.java
@@ -0,0 +1,11 @@
+package programmers.algorithm.lv1;
+
+public class EvenAndOdd {
+    public String solution(int num) {
+        if (num % 2 == 0)
+            return "Even";
+        else
+            return "Odd";
+    }
+}
+// Resource : https://school.programmers.co.kr/learn/courses/30/lessons/12937
