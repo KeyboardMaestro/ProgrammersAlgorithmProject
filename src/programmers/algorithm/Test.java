c72d09ecee8dd6f52539d974a1a26a8b9b6c580f
Sungwon Jude Park <u7079876@anu.edu.au>
2023-02-21 16:10:02 +1100

    Test Commit

--- src/programmers/algorithm/Test.java
+++ src/programmers/algorithm/Test.java
@@ -1,22 +1,7 @@
 package programmers.algorithm;
 
-import java.util.*;
-
-class Test {
+public class Test{
     public static void main(String[] args) {
-        int[] input = {600, 400, 300, 200, 700, 800, 100, 900};
-        solution("1 2 Z 3");
-    }
-    public static int solution(String s) {
-        int answer = 0;
-        String[] arr = s.split(" ");
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("Z")) {
-                answer -= Integer.parseInt(arr[i - 1]);
-            } else {
-                answer += Integer.parseInt(arr[i]);
-            }
-        }
-        return answer;
+        System.out.println("Test");
     }
 }
