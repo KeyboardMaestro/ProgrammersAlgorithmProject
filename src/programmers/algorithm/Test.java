fdba73257089f641d298a84e56cd0412acf8c17f
Sungwon Jude Park <psw0616a@naver.com>
2023-02-21 16:15:01 +1100

    Test Commit

--- src/programmers/algorithm/Test.java
+++ src/programmers/algorithm/Test.java
@@ -2,6 +2,6 @@ package programmers.algorithm;
 
 public class Test{
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test!");
     }
 }
